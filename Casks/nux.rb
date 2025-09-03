d436790218b7f102291c7d3d8ab647a1ea65a859
Laksh Bharani <laksh.bharani@gmail.com>
2025-09-03 01:49:16 -0400

    Update nux SHA256 for v1.0.0 release

--- Casks/nux.rb
+++ Casks/nux.rb
@@ -2,7 +2,7 @@ cask "nux" do
   desc "Native macOS terminal built with SwiftUI - calm, fast experience with AI integration"
   homepage "https://github.com/LakshBharani/nux"
   version "1.0.0"
-  sha256 "YOUR_SHA256_HERE" # You'll need to update this after creating a release
+  sha256 "a7141e4bfb09073c8c08f068d151eedd16cd06182fc8eac8bb567bc4261faff2" # You'll need to update this after creating a release
   
   url "https://github.com/LakshBharani/nux/releases/download/v#{version}/nux-#{version}.zip"
   name "nux"
